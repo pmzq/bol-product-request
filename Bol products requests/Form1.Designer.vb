17873b4825cc92fbcc008478aa4db9eb69786d6b
ferdihorst@gmail.com <Onxw1234>
2017-07-08 21:05:37 +0200

    first final version

--- Bol products requests/Form1.Designer.vb
+++ Bol products requests/Form1.Designer.vb
@@ -26,11 +26,51 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtUrl1 = New System.Windows.Forms.TextBox()
         Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.txtAlt1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.Label20 = New System.Windows.Forms.Label()
+        Me.txtAlt2 = New System.Windows.Forms.TextBox()
+        Me.txtUrl2 = New System.Windows.Forms.TextBox()
+        Me.txtAlt3 = New System.Windows.Forms.TextBox()
+        Me.txtUrl3 = New System.Windows.Forms.TextBox()
+        Me.txtAlt4 = New System.Windows.Forms.TextBox()
+        Me.txtUrl4 = New System.Windows.Forms.TextBox()
+        Me.txtAlt5 = New System.Windows.Forms.TextBox()
+        Me.txtUrl5 = New System.Windows.Forms.TextBox()
+        Me.txtAlt6 = New System.Windows.Forms.TextBox()
+        Me.txtUrl6 = New System.Windows.Forms.TextBox()
+        Me.txtAlt7 = New System.Windows.Forms.TextBox()
+        Me.txtUrl7 = New System.Windows.Forms.TextBox()
+        Me.txtAlt8 = New System.Windows.Forms.TextBox()
+        Me.txtUrl8 = New System.Windows.Forms.TextBox()
+        Me.txtAlt9 = New System.Windows.Forms.TextBox()
+        Me.txtUrl9 = New System.Windows.Forms.TextBox()
+        Me.txtAlt10 = New System.Windows.Forms.TextBox()
+        Me.txtUrl10 = New System.Windows.Forms.TextBox()
+        Me.txtTag = New System.Windows.Forms.TextBox()
+        Me.Label21 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(307, 200)
+        Me.Button1.Location = New System.Drawing.Point(839, 168)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -40,7 +80,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(39, 36)
+        Me.Label1.Location = New System.Drawing.Point(29, 17)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(52, 17)
         Me.Label1.TabIndex = 1
@@ -48,24 +88,384 @@ Partial Class Form1
         '
         'txtUrl1
         '
-        Me.txtUrl1.Location = New System.Drawing.Point(97, 31)
+        Me.txtUrl1.Location = New System.Drawing.Point(99, 12)
         Me.txtUrl1.Name = "txtUrl1"
         Me.txtUrl1.Size = New System.Drawing.Size(558, 22)
         Me.txtUrl1.TabIndex = 2
         '
         'txtOutput
         '
-        Me.txtOutput.Location = New System.Drawing.Point(155, 264)
+        Me.txtOutput.Location = New System.Drawing.Point(839, 12)
         Me.txtOutput.Multiline = True
         Me.txtOutput.Name = "txtOutput"
         Me.txtOutput.Size = New System.Drawing.Size(431, 150)
         Me.txtOutput.TabIndex = 3
         '
+        'txtAlt1
+        '
+        Me.txtAlt1.Location = New System.Drawing.Point(99, 40)
+        Me.txtAlt1.Name = "txtAlt1"
+        Me.txtAlt1.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Alt tag"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Alt tag"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 75)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "URL 1:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 162)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 17)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Alt tag"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 133)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 17)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "URL 1:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 220)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 17)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Alt tag"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(29, 191)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(52, 17)
+        Me.Label8.TabIndex = 10
+        Me.Label8.Text = "URL 1:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(29, 278)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 17)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Alt tag"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(29, 249)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(52, 17)
+        Me.Label10.TabIndex = 12
+        Me.Label10.Text = "URL 1:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(29, 394)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(48, 17)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = "Alt tag"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(29, 365)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(52, 17)
+        Me.Label12.TabIndex = 14
+        Me.Label12.Text = "URL 1:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(29, 307)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(52, 17)
+        Me.Label13.TabIndex = 14
+        Me.Label13.Text = "URL 1:"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(29, 336)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(48, 17)
+        Me.Label14.TabIndex = 15
+        Me.Label14.Text = "Alt tag"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(29, 452)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(48, 17)
+        Me.Label15.TabIndex = 17
+        Me.Label15.Text = "Alt tag"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(29, 423)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(52, 17)
+        Me.Label16.TabIndex = 16
+        Me.Label16.Text = "URL 1:"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(29, 510)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(48, 17)
+        Me.Label17.TabIndex = 19
+        Me.Label17.Text = "Alt tag"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(29, 481)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(52, 17)
+        Me.Label18.TabIndex = 18
+        Me.Label18.Text = "URL 1:"
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(29, 568)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(48, 17)
+        Me.Label19.TabIndex = 21
+        Me.Label19.Text = "Alt tag"
+        '
+        'Label20
+        '
+        Me.Label20.AutoSize = True
+        Me.Label20.Location = New System.Drawing.Point(29, 539)
+        Me.Label20.Name = "Label20"
+        Me.Label20.Size = New System.Drawing.Size(52, 17)
+        Me.Label20.TabIndex = 20
+        Me.Label20.Text = "URL 1:"
+        '
+        'txtAlt2
+        '
+        Me.txtAlt2.Location = New System.Drawing.Point(99, 103)
+        Me.txtAlt2.Name = "txtAlt2"
+        Me.txtAlt2.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt2.TabIndex = 23
+        '
+        'txtUrl2
+        '
+        Me.txtUrl2.Location = New System.Drawing.Point(99, 75)
+        Me.txtUrl2.Name = "txtUrl2"
+        Me.txtUrl2.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl2.TabIndex = 22
+        '
+        'txtAlt3
+        '
+        Me.txtAlt3.Location = New System.Drawing.Point(99, 161)
+        Me.txtAlt3.Name = "txtAlt3"
+        Me.txtAlt3.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt3.TabIndex = 25
+        '
+        'txtUrl3
+        '
+        Me.txtUrl3.Location = New System.Drawing.Point(99, 133)
+        Me.txtUrl3.Name = "txtUrl3"
+        Me.txtUrl3.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl3.TabIndex = 24
+        '
+        'txtAlt4
+        '
+        Me.txtAlt4.Location = New System.Drawing.Point(99, 217)
+        Me.txtAlt4.Name = "txtAlt4"
+        Me.txtAlt4.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt4.TabIndex = 27
+        '
+        'txtUrl4
+        '
+        Me.txtUrl4.Location = New System.Drawing.Point(99, 189)
+        Me.txtUrl4.Name = "txtUrl4"
+        Me.txtUrl4.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl4.TabIndex = 26
+        '
+        'txtAlt5
+        '
+        Me.txtAlt5.Location = New System.Drawing.Point(99, 277)
+        Me.txtAlt5.Name = "txtAlt5"
+        Me.txtAlt5.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt5.TabIndex = 29
+        '
+        'txtUrl5
+        '
+        Me.txtUrl5.Location = New System.Drawing.Point(99, 249)
+        Me.txtUrl5.Name = "txtUrl5"
+        Me.txtUrl5.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl5.TabIndex = 28
+        '
+        'txtAlt6
+        '
+        Me.txtAlt6.Location = New System.Drawing.Point(99, 333)
+        Me.txtAlt6.Name = "txtAlt6"
+        Me.txtAlt6.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt6.TabIndex = 31
+        '
+        'txtUrl6
+        '
+        Me.txtUrl6.Location = New System.Drawing.Point(99, 305)
+        Me.txtUrl6.Name = "txtUrl6"
+        Me.txtUrl6.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl6.TabIndex = 30
+        '
+        'txtAlt7
+        '
+        Me.txtAlt7.Location = New System.Drawing.Point(99, 393)
+        Me.txtAlt7.Name = "txtAlt7"
+        Me.txtAlt7.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt7.TabIndex = 33
+        '
+        'txtUrl7
+        '
+        Me.txtUrl7.Location = New System.Drawing.Point(99, 365)
+        Me.txtUrl7.Name = "txtUrl7"
+        Me.txtUrl7.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl7.TabIndex = 32
+        '
+        'txtAlt8
+        '
+        Me.txtAlt8.Location = New System.Drawing.Point(99, 451)
+        Me.txtAlt8.Name = "txtAlt8"
+        Me.txtAlt8.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt8.TabIndex = 35
+        '
+        'txtUrl8
+        '
+        Me.txtUrl8.Location = New System.Drawing.Point(99, 423)
+        Me.txtUrl8.Name = "txtUrl8"
+        Me.txtUrl8.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl8.TabIndex = 34
+        '
+        'txtAlt9
+        '
+        Me.txtAlt9.Location = New System.Drawing.Point(99, 509)
+        Me.txtAlt9.Name = "txtAlt9"
+        Me.txtAlt9.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt9.TabIndex = 37
+        '
+        'txtUrl9
+        '
+        Me.txtUrl9.Location = New System.Drawing.Point(99, 481)
+        Me.txtUrl9.Name = "txtUrl9"
+        Me.txtUrl9.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl9.TabIndex = 36
+        '
+        'txtAlt10
+        '
+        Me.txtAlt10.Location = New System.Drawing.Point(99, 567)
+        Me.txtAlt10.Name = "txtAlt10"
+        Me.txtAlt10.Size = New System.Drawing.Size(558, 22)
+        Me.txtAlt10.TabIndex = 39
+        '
+        'txtUrl10
+        '
+        Me.txtUrl10.Location = New System.Drawing.Point(99, 539)
+        Me.txtUrl10.Name = "txtUrl10"
+        Me.txtUrl10.Size = New System.Drawing.Size(558, 22)
+        Me.txtUrl10.TabIndex = 38
+        '
+        'txtTag
+        '
+        Me.txtTag.Location = New System.Drawing.Point(848, 243)
+        Me.txtTag.Name = "txtTag"
+        Me.txtTag.Size = New System.Drawing.Size(177, 22)
+        Me.txtTag.TabIndex = 40
+        '
+        'Label21
+        '
+        Me.Label21.AutoSize = True
+        Me.Label21.Location = New System.Drawing.Point(805, 246)
+        Me.Label21.Name = "Label21"
+        Me.Label21.Size = New System.Drawing.Size(37, 17)
+        Me.Label21.TabIndex = 41
+        Me.Label21.Text = "Tag:"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(849, 478)
+        Me.ClientSize = New System.Drawing.Size(1282, 655)
+        Me.Controls.Add(Me.Label21)
+        Me.Controls.Add(Me.txtTag)
+        Me.Controls.Add(Me.txtAlt10)
+        Me.Controls.Add(Me.txtUrl10)
+        Me.Controls.Add(Me.txtAlt9)
+        Me.Controls.Add(Me.txtUrl9)
+        Me.Controls.Add(Me.txtAlt8)
+        Me.Controls.Add(Me.txtUrl8)
+        Me.Controls.Add(Me.txtAlt7)
+        Me.Controls.Add(Me.txtUrl7)
+        Me.Controls.Add(Me.txtAlt6)
+        Me.Controls.Add(Me.txtUrl6)
+        Me.Controls.Add(Me.txtAlt5)
+        Me.Controls.Add(Me.txtUrl5)
+        Me.Controls.Add(Me.txtAlt4)
+        Me.Controls.Add(Me.txtUrl4)
+        Me.Controls.Add(Me.txtAlt3)
+        Me.Controls.Add(Me.txtUrl3)
+        Me.Controls.Add(Me.txtAlt2)
+        Me.Controls.Add(Me.txtUrl2)
+        Me.Controls.Add(Me.Label19)
+        Me.Controls.Add(Me.Label20)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.Label18)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtAlt1)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtOutput)
         Me.Controls.Add(Me.txtUrl1)
         Me.Controls.Add(Me.Label1)
@@ -81,4 +481,44 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents txtUrl1 As TextBox
     Friend WithEvents txtOutput As TextBox
+    Friend WithEvents txtAlt1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents txtAlt2 As TextBox
+    Friend WithEvents txtUrl2 As TextBox
+    Friend WithEvents txtAlt3 As TextBox
+    Friend WithEvents txtUrl3 As TextBox
+    Friend WithEvents txtAlt4 As TextBox
+    Friend WithEvents txtUrl4 As TextBox
+    Friend WithEvents txtAlt5 As TextBox
+    Friend WithEvents txtUrl5 As TextBox
+    Friend WithEvents txtAlt6 As TextBox
+    Friend WithEvents txtUrl6 As TextBox
+    Friend WithEvents txtAlt7 As TextBox
+    Friend WithEvents txtUrl7 As TextBox
+    Friend WithEvents txtAlt8 As TextBox
+    Friend WithEvents txtUrl8 As TextBox
+    Friend WithEvents txtAlt9 As TextBox
+    Friend WithEvents txtUrl9 As TextBox
+    Friend WithEvents txtAlt10 As TextBox
+    Friend WithEvents txtUrl10 As TextBox
+    Friend WithEvents txtTag As TextBox
+    Friend WithEvents Label21 As Label
 End Class
--- Bol products requests/Form1.vb
+++ Bol products requests/Form1.vb
@@ -5,24 +5,83 @@ Imports Newtonsoft.Json.Linq
 
 Public Class Form1
 
+    Public API_key, Bol_ID, Product_id As String
+    Dim request As HttpWebRequest
+    Dim response As HttpWebResponse = Nothing
+    Dim reader As StreamReader
+    Dim Title, Description, key, Prijs As String
+    Dim Rating As Long
+    Dim Image_url As String = ""
+    Dim Image_urls As Object
+    Dim Output As String = ""
+    Dim Url, Alt_tag As String
+    Dim Bol_tag As String
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim Url As String
-        Dim API_key As String
+
+
+        Dim Counter As Integer
+
+        Dim Url_boxes As New List(Of TextBox)() From {
+            txtUrl1,
+            txtUrl2,
+            txtUrl3,
+            txtUrl4,
+            txtUrl5,
+            txtUrl6,
+            txtUrl7,
+            txtUrl7,
+            txtUrl8,
+            txtUrl9,
+            txtUrl10
+        }
+
+        Dim Alt_boxes As New List(Of TextBox)() From {
+            txtAlt1,
+            txtAlt2,
+            txtAlt3,
+            txtAlt4,
+            txtAlt5,
+            txtAlt6,
+            txtAlt7,
+            txtAlt7,
+            txtAlt8,
+            txtAlt9,
+            txtAlt10
+        }
 
         API_key = "66744DFB331F4D9E838E38D48A2052D6"
-        Url = txtUrl1.Text
+        Bol_ID = "13464"
+        Bol_tag = txtTag.Text
 
-        Call get_product(API_key, Url)
+        Counter = 1
+
+        For Each tb As TextBox In Url_boxes
+            Url = tb.Text
+            If Url <> "" Then
+                Alt_tag = Alt_boxes(Counter).Text
+
+                Dim parts As String() = Url.Split(New Char() {"/"c})
+
+                ' Loop through result strings with For Each.
+                Product_id = parts(6)
+
+                Call Get_product()
+                Call Compose_html()
+            Else
+            End If
+        Next
+
+
+
 
 
     End Sub
 
-    Sub get_product(API_key, Product_id)
+    Sub Get_product()
 
-        Dim request As HttpWebRequest
-        Dim response As HttpWebResponse = Nothing
-        Dim reader As StreamReader
+
 
         request = DirectCast(WebRequest.Create("https://api.bol.com/catalog/v4/products/" & Product_id & "?apikey=" & API_key & "&includeAttributes=true&format=json"), HttpWebRequest)
 
@@ -38,9 +97,39 @@ Public Class Form1
         'MsgBox(firstItem)
 
         Dim jResults As Object = JObject.Parse(rawresp)
-        txtOutput.Text = If(jResults("products") Is Nothing, "", jResults("products").ToString())
-        MsgBox(jResults("products")(0)("id"))
+        'txtOutput.Text = If(jResults("products") Is Nothing, "", jResults("products").ToString())
+        Product_id = jResults("products")(0)("id")
+        Title = jResults("products")(0)("title")
+        Description = jResults("products")(0)("longDescription")
+        Prijs = jResults("products")(0)("price")
+        Rating = jResults("products")(0)("rating")
+        Image_urls = jResults("products")(0)("images")
 
+        For Each image In Image_urls
+
+            key = image("key")
+            If key = "XL" Then
+                Image_url = image("url")
+            ElseIf key = "L" Then
+                Image_url = image("url")
+            End If
+
+        Next
+
+
+        'Url = jResults("urls")("DESKTOP")
+
+    End Sub
+
+    Sub Compose_html()
+
+        Output = Output & "&nbsp;&nbsp;<span style=""font-size: 14pt;""><strong><a href=""https://partnerprogramma.bol.com/click/click?p=1&amp;t=url&amp;s=" & Bol_ID & "&amp;f=TXL&amp;url=https%3A%2F%2Fwww.bol.com%2Fnl%2Fp%2F" & Title & "%2F" & Product_id & "&amp;name=" & Bol_tag & ">" & Title & "</a></strong></span>
+                
+                <a href=""" & Image_url & """><img class=""size-thumbnail wp-image-276 alignleft"" src=""" & Image_url & """ alt=""" & Alt_tag & """ width=""150"" height=""150"" /></a>" & Description & "
+
+                Reviews Bol.com [usr " & Rating / 10 & "]"
+
+        txtOutput.Text = Output
 
     End Sub
 
